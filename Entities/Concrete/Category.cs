f4068abba048cfaf9cfeb8d4630d5ccfbf62b5ab
Sümeyye KILIÇ <smyyekilic@gmail.com>
2021-02-06 20:48:52 +0300

    oluşturduğum taslak

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Çıplak Class Kalmasın
+    public class Category : IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product
+    {
+        //public "bu class'a diğer katmanlar da ulaşabilsin" demek 
+        //çünkü data access ürünü ekleyecek
+        //business ürünü kontrol edecek
+        //console ürünü gösterecek.
+        //entities bu üç katmanı kullanacağı için burayı public yapmamızınn sebebi bu
+
+
+        //bir classın defoult'ı internal'dır. internal sadece entities erşebilir demek.
+
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
