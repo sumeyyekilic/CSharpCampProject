f8087434666a60efeaf31c18c359caee44277afa
Sümeyye KILIÇ <smyyekilic@gmail.com>
2021-02-21 22:35:18 +0300

    code refactoring.

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //IEntity implement eden class bir veritabanı tablosudur.
-    //concretae tablosundaki erşeyi IEntity olarak implamente edeceksin
+    //concretae tablosundaki herşeyi IEntity olarak implamente edeceksin
     public interface IEntity
     {
     }
