9d1d5c9c548984dacf8bd26ca8779d0beaaa541d
Sümeyye KILIÇ <smyyekilic@gmail.com>
2021-02-22 01:10:16 +0300

    entity katmanı çalışmaları.

---
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.DataAccess.EntityFramework
+{
+    class EfEntityRepositoryBase<TEntity, TContext>
+        where TEntity: class , IEntity, new()
+        where TContext: DbContext , new() //Dbcontext entity fw'den gelir.
+    {
+
+    }
+}
