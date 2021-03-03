10ca8f67f78cc4787c91d6e523d563fffc417cff
Sümeyye KILIÇ <smyyekilic@gmail.com>
2021-03-04 00:52:01 +0300

    AOP tekniği interceptor altyapısı hazır

---
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,25 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Core.Utilities.Interceptors
+{
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>  //class ın attr oku
+                (true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);  //ilgili metodun attr oku
+            classAttributes.AddRange(methodAttributes); //onları bir listeye koy
+                                                        //şu hareket * otomatik olarak sistemdeki tüm metotları loglamaya dahil et. 3 sene proje geliştyse bunu eklemen yeterli. şimdilik loglama altyapım olmadığı içiön kaldırdım.
+                                                        //classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger)));  //çalışma sırasına göre de sırala
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
